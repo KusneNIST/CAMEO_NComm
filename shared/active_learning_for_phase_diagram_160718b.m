b336a0d8faeae26f2fb5a04e45abcce4febc699b
KusneNIST <aaron.kusne@nist.gov>
2021-03-09 14:22:30 -0500

    Update active_learning_for_phase_diagram_160718b.m

--- shared/active_learning_for_phase_diagram_160718b.m
+++ shared/active_learning_for_phase_diagram_160718b.m
@@ -28,9 +28,8 @@ function [query, certainty, risks, full_cluster_labels, fu] = active_learning_fo
 
 % A. Gilad Kusne, NIST, aaron.kusne@nist.gov, Release 8/01/2020
 % If using this work for a publication, please cite:
-% Kusne, A. G., et al. "On-the-fly Closed-loop Autonomous Materials
-% Discovery via Bayesian Active Learning." arXiv preprint arXiv:2006.06141
-% (2020).
+% Kusne, A. Gilad, et al. "On-the-fly closed-loop materials discovery
+% via Bayesian active learning." Nature communications 11.1 (2020): 1-11.
 
 Nl = size(S,1)-size(XY,1);
 N = size(S,1);
