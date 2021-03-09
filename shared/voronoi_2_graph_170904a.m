3c0b6ec35c46d17deaf6e2a74bac70a0711e67dd
KusneNIST <aaron.kusne@nist.gov>
2021-03-09 14:27:43 -0500

    Update voronoi_2_graph_170904a.m

--- shared/voronoi_2_graph_170904a.m
+++ shared/voronoi_2_graph_170904a.m
@@ -27,9 +27,8 @@ function [S] = voronoi_2_graph_170904a(N, voronoi_points_separated)
 
 % A. Gilad Kusne, NIST, aaron.kusne@nist.gov, Release 8/01/2020
 % If using this work for a publication, please cite:
-% Kusne, A. G., et al. "On-the-fly Closed-loop Autonomous Materials
-% Discovery via Bayesian Active Learning." arXiv preprint arXiv:2006.06141
-% (2020).
+% Kusne, A. Gilad, et al. "On-the-fly closed-loop materials discovery
+% via Bayesian active learning." Nature communications 11.1 (2020): 1-11.
 
 S = zeros(N,N);
 for i = 1:size(voronoi_points_separated)
